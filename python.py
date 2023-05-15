fd0c884a12ac5000eaebe509b01c973cb6d4fbf6
Ondřej Hájek <hajek@gjkt.local>
2023-05-15 14:36:34 +0200

    fix

--- python.py
+++ python.py
@@ -1,10 +1,24 @@
+from tkinter import *
+
 file = open("soubor.txt", "r")
 size = file.readline().split(" ")
 rowCount = int(size[0])
 columnCount = int(size[1])
 
-for _ in range(rowCount):
-    print(file.readline())
+win=Tk()
+win.geometry("700x350")
+canvas=Canvas(win, width=700, height=350)
+canvas.pack()
 
-for _ in range(columnCount):
-    print(file.readline())
+for _ in range(rowCount):
+    for id, item in enumerate(file.readline().split(" ")):
+        num = id+1
+        if item:
+            canvas.create_line(20,20,20,80)
+
+
+
+
+
+
+win.mainloop()
