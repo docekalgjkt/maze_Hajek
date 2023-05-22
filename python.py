2f3f65c6397bd2761e45eba636936cb373b7c0cd
OndrejHj04 <ondra.hajku@seznam.cz>
2023-05-22 13:29:55 +0200

    feat(dashboard): created canvas

--- python.py
+++ python.py
@@ -4,17 +4,22 @@ file = open("soubor.txt", "r")
 size = file.readline().split(" ")
 rowCount = int(size[0])
 columnCount = int(size[1])
+print(rowCount)
+def generate():
+    print("hovno")
+
 
 win=Tk()
-win.geometry("700x350")
-canvas=Canvas(win, width=700, height=350)
+win.geometry("700x500")
+frame1 = Frame(win, width=200, height=500,bg="red")
+frame1.place(x=0, y=0)
+btn1 = Button(frame1, text="Generovat", bg="blue", command=generate)
+btn1.place(x=0,y=0)
+frame2 = Frame(win, width=500, height=500)
+frame2.place(x=200, y=0)
+canvas=Canvas(frame2, width=500, height=500, bg="blue")
 canvas.pack()
 
-for _ in range(rowCount):
-    for id, item in enumerate(file.readline().split(" ")):
-        num = id+1
-        if item:
-            canvas.create_line(20,20,20,80)
 
 
 
